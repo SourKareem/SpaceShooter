1e4f8c42fc0190227d6c45a3870ebdf2983c4dbf
Caleb Cosler <calebcosler123@gmail.com>
2025-12-12 23:47:32 -0800

    12-12

--- code/main.py
+++ code/main.py
@@ -1,2 +1,31 @@
 import pygame
 
+# general setup
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+pygame.display.set_caption('Space Shooter')
+running = True
+
+# plain surface
+surf = pygame.Surface((100,200))
+surf.fill('orange')
+x = 100
+
+# importing an image
+
+
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    # draw the game
+    display_surface.fill('darkgray')
+    x += 0.1
+    display_surface.blit(surf, (x,150))
+    pygame.display.update()
+
+pygame.quit()
